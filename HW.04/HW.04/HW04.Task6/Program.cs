1412fb9f1accf99547569264d7354c546e99c354
Ivan Yermilov <van.ermilov@gmail.com>
2021-02-14 20:50:29 +0300

    Revert "Revert "Added solutions of tasks in the homework #3""
    This reverts commit 177214faf96dc96d2edfcfb6d7b20f88240e0f7f.

---
+++ HW.04/HW.04/HW04.Task6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace HW04.Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; ;)
+            {
+                Console.Write("Input symbol on your keyboard (Press Q to quit): ");
+                char Char = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                if (Char == 'Q')
+                {
+                    Console.WriteLine("Thanks for playing :)");
+                    return;
+                }
+                else
+                {
+                    switch (Char)
+                    {
+                        case 'w':
+                            Console.WriteLine($"Сharacter is moving forward.");
+                            break;
+                        case 'a':
+                            Console.WriteLine($"Сharacter is moving left.");
+                            break;
+                        case 's':
+                            Console.WriteLine($"Сharacter is moving back.");
+                            break;
+                        case 'd':
+                            Console.WriteLine($"Сharacter is moving right.");
+                            break;
+                        default:
+                            Console.WriteLine("Сharacter doesn't move.");
+                            break;
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
