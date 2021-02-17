d6b02a1b05d221848dc98640e1336194334cb83a
Ivan Yermilov <van.ermilov@gmail.com>
2021-02-17 11:49:39 +0300

    Added solutions for Task#1 and Task#2 of homework #5

---
+++ HW.05/HW.05.Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HW._05.Task1
+{
+    class Program
+    {
+        static int GetSum(int value)
+        {
+            int sum = 0;
+            for (int i = 1; i <= value; i++)
+            {
+                sum += i; 
+            }
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            int number;
+            for (; ; )
+            {
+                Console.Write("Input positive number: ");
+                string strNumber = Console.ReadLine();
+                if (!int.TryParse(strNumber, out int parsedValue)) Console.WriteLine("Programm cannot parse inputted data. \n");
+                else
+                {
+                    number = Math.Abs(parsedValue);
+                    Console.WriteLine();
+                    break;
+                }
+
+            }
+
+            Console.Write($"The result of summing is: {Program.GetSum(number)}");
+        }
+    }
+}
---
+++ HW.05/HW.05/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace HW._05.Task2
+{
+    class Program
+    {
+        public static int TakeLastDigit(int number)
+        {
+            if (number < 10) return number;
+            return number % 10;
+        }
+
+        static void Main(string[] args)
+        {
+            int number;
+
+            for (; ;)
+            {
+                Console.Write("Input positive number: ");
+                string strNumber = Console.ReadLine();
+                if (!int.TryParse(strNumber, out int parsedValue)) Console.WriteLine("Programm cannot parse inputted data. \n");
+                else 
+                {
+                    number = Math.Abs(parsedValue);
+                    Console.WriteLine();
+                    break;
+                }
+            }
+
+            for (int i = 1, j = number; i <= number; i++, j--)
+            {
+                for (int n = 1, m = 1; m <= j; n++)
+                {
+                    if (n < i) Console.Write(" ");
+                    else
+                    {
+                        Console.Write(Program.TakeLastDigit(i) + " ");
+                        m++;
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
