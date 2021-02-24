f83caa187b8a704713d952491ed9948aa59ac6ed
Ivan Yermilov <van.ermilov@gmail.com>
2021-02-24 13:33:00 +0300

    Added solution for optional task

---
+++ HW.07/HW.07.Task4(Optional)/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HW._07.Task4_Optional_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am" +18 + ".");
+            Console.WriteLine("I am" +18.ToString() + ".");
+            Console.WriteLine($"I am { 18}.");
+        }
+    }
+}
