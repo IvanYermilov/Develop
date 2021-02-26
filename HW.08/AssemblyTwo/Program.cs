00f9166a74a2e5b3af41790f2d65cc799da1f464
Ivan Yermilov <van.ermilov@gmail.com>
2021-02-26 23:01:40 +0300

    added description for protected internal method calling

--- HW.08/AssemblyTwo/Program.cs
+++ HW.08/AssemblyTwo/Program.cs
@@ -11,7 +11,8 @@ namespace AssemblyTwo
             //SportBike sportBike = new SportBike(); can't create instance of SportBike class because it has Internal access modifier
             moto.publicModel = 15;
             //moto.StartEngine(); StartEngine(protected internal method) doesn't accessible because of class Program doesn't derived 
-            //                    from Motorcycle and because of class Program doesn't basic class where method declared
+            //                    from Motorcycle and because of class Program doesn't basic class where method declared. and finally
+            //                    StartEngine declared in another assembly(AssemblyOne)
             //moto.StopEngine(); StartEngine(internal method) doesn't accessible because of it declared in AssemblyOne(Motorcycle class)
             //moto.vin = 40; can't set value because it available in Motorcycle and SportBike classes only that declared in AssemblyOne.
         }
