c31891a4f6973a5647844a917e71a83e187d25ef
Ivan Yermilov <van.ermilov@gmail.com>
2021-02-27 20:14:00 +0300

    Added solution for optional task 4

---
+++ HW.08/HW.08.Task4Optional)/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HW._08.Task4Optional_
+{
+    class Program
+    {
+        int aField = 10;
+        const int aConst = 20;
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
