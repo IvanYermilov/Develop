74fd101ee8fea6cf98d51b40f412b8c6614d2e51
Ivan Yermilov <van.ermilov@gmail.com>
2021-03-20 23:25:02 +0300

    Added solution for homework 11

---
+++ HW.11/HW.11.Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using Serilog;
+using Serilog.Events;
+using System;
+using Task_1.UserInterface;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File(@".\log\log-.txt", LogEventLevel.Information, rollingInterval: RollingInterval.Hour)
+                .CreateLogger();
+
+            Type t = typeof(Program);
+            Log.Information($"Application {t.Namespace} has been started");
+            ConsoleIO consoleIO = new ConsoleIO();
+            while(!consoleIO.IsProgramMustStop())
+            {
+                consoleIO.PickDatabaseOperation();
+            }
+            Log.Information($"Application {t.Namespace} has been stopped");
+        }
+    }
+}
---
+++ HW.11/HW.11.Task1/UserInterfaceLayer/ConsoleIO.cs
@@ -0,0 +1,161 @@
+﻿using Serilog;
+using System;
+using Task_1.BusinessLogic;
+using Task_1.DataAccessLayer;
+using System.Collections;
+
+namespace Task_1.UserInterface
+{
+    class ConsoleIO
+    {
+        public IRepository Repository { get; set; }
+
+        public ConsoleIO()
+        {
+            Repository = new MotorcycleRepository();
+        }
+
+        public void CreateMotorcycle(Motorcycle motorcycle)
+        {
+            Repository.Create(motorcycle);
+        }
+
+        public void GetMotorcycleById(int id)
+        {
+            Motorcycle motorcycle = Repository.GetById(id);
+            if (motorcycle != null)
+            {
+                Log.Information($"Motorcycle displaying process has been started.");
+                PrintMotorcycle(motorcycle);
+            }
+            else Console.WriteLine($"Tnere is no motorcycle with id = {id}");
+        }
+
+        public void GetAllMotorcycles()
+        {
+            ArrayList motorcycleList = Repository.GetAll();
+            if (motorcycleList != null)
+            {
+                Log.Information($"Motorcycles displaying process has been started.");
+                foreach (Motorcycle motorcycle in motorcycleList)
+                {
+                    PrintMotorcycle(motorcycle);
+                }
+            }
+            else Console.WriteLine("There is nothing to display");
+        }
+
+        public void UpadateMotorcycle(int id, Motorcycle motorcycle)
+        {
+            Repository.Update(id, motorcycle);
+        }
+
+        public void DeleteMotorcycle(int id)
+        {
+            Repository.Delete(id);
+        }
+
+        public bool IsProgramMustStop()
+        {
+            Console.Write("Input 'q' if you want to stop program:");
+            char inputtedChar = Console.ReadKey().KeyChar;
+            switch (inputtedChar)
+            {
+                case ('q'):
+                    Console.WriteLine();
+                    return true;
+                default:
+                    Console.WriteLine();
+                    return false;
+            }
+        }
+
+        public void PickDatabaseOperation()
+        {
+            int motorcycleId;
+            Console.WriteLine("Input one of five operation you can do with Data Base " +
+                "(create; readAll, readById, update, delete):");
+            for (; ; )
+            {
+                string userInput = Console.ReadLine();
+                switch (userInput)
+                {
+                    case ("create"):
+                        CreateMotorcycle(GetMotorcycle());
+                        return;
+                    case ("readAll"):
+                        GetAllMotorcycles();
+                        return;
+                    case ("readById"):
+                        Console.Write("Input ID of motorcycle you want to read: ");
+                        motorcycleId = GetMotorcycleId();
+                        GetMotorcycleById(motorcycleId);
+                        return;
+                    case ("update"):
+                        Console.Write("Input ID of motorcycle you want to upadte: ");
+                        motorcycleId = GetMotorcycleId();
+                        Console.WriteLine($"Input new information about motorcycle with ID-{motorcycleId}");
+                        UpadateMotorcycle(motorcycleId, GetMotorcycle());
+                        return;
+                    case ("delete"):
+                        Console.Write("Input ID of motorcycle you want to delete: ");
+                        motorcycleId = GetMotorcycleId();
+                        DeleteMotorcycle(motorcycleId);
+                        return;
+                    default:
+                        Console.Write("Program cannot perform this operation with database. Try to input operation again:");
+                        break;
+                }
+            }
+        }
+
+        public Motorcycle GetMotorcycle()
+        {
+            Console.Write("Input motorcycle Brand: ");
+            string brand = Console.ReadLine();
+            Console.Write("Input motorcycle Model: ");
+            string model = Console.ReadLine();
+            int year = GetMotorcycleYear();
+            int odometer = GetMotorcycleOdometer();
+            return new Motorcycle(brand, model, year, odometer);
+        }
+
+        public static void PrintMotorcycle(Motorcycle motorcycle)
+        {
+            Console.WriteLine($"Motorcycle info: ID-{motorcycle.Id}, Brand-{motorcycle.Brand}, Model-{motorcycle.Model}, " +
+                $"Year made-{motorcycle.Year}, Odometer value-{motorcycle.Odometer}");
+        }
+
+        public static int GetMotorcycleId()
+        {
+            for (; ; )
+            {
+                string motorcycleIdStr = Console.ReadLine();
+                if (int.TryParse(motorcycleIdStr, out int motorcycleId)) return motorcycleId;
+                Console.Write("Programm cannot parse inputted data. Try again: ");
+            }
+        }
+
+        public static int GetMotorcycleYear()
+        {
+            for (; ; )
+            {
+                Console.Write("Input motorcycle year made: ");
+                string motorcycleYearMadeStr = Console.ReadLine();
+                if (int.TryParse(motorcycleYearMadeStr, out int motorcycleYearMade)) return motorcycleYearMade;
+                Console.WriteLine("Programm cannot parse inputted data. Try again: ");
+            }
+        }
+        public static int GetMotorcycleOdometer()
+        {
+            for (; ; )
+            {
+                Console.Write("Input odometer value: ");
+                string motorcycleOdometerStr = Console.ReadLine();
+                if (int.TryParse(motorcycleOdometerStr, out int motorcycleOdometer)) return motorcycleOdometer;
+                Console.WriteLine("Programm cannot parse inputted data. Try again: ");
+            }
+        }
+
+    }
+}
