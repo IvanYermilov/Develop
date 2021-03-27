a7c7afdf656a68bb0ab0552204a578729d0e510b
Ivan Yermilov <van.ermilov@gmail.com>
2021-03-27 17:35:51 +0300

    Initial commit

---
+++ SushiBot/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SushiBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 0;
+            Console.CursorVisible = false;
+            string[] strArr = {"one", "two", "three"};
+            for (; ; )
+            {
+                for (int i = 0; i < strArr.Length; i++)
+                {
+                    if (i == index)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.BackgroundColor = ConsoleColor.White;
+                    }
+                    Console.WriteLine(strArr[i]);
+                }
+                ConsoleKey c = Console.ReadKey().Key;
+                Console.Clear();
+                if (c == ConsoleKey.UpArrow) index++;
+
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
